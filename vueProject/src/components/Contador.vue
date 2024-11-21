0fb488f6de5399579ad39c54c4928ac51cf39520
Focus <el07385@gmail.com>
2024-11-21 14:06:25 -0500

    no message

---
+++ vueProject/src/components/Contador.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hola Mundo</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">incrementar</button>
+  <button @click="Decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+};
+const Decrementar = () => {
+  contador.value--;
+};
+</script>
+
+<style scoped></style>
---
+++ vueProject/src/views/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Contador></Contador>
+</template>
+
+<script setup>
+import Contador from "../components/Contador.vue";
+</script>
+
+<style scoped></style>
