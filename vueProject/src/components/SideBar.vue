8583b5b16cea90b02038109d44df0e5c38325542
Focus <el07385@gmail.com>
2024-11-28 17:22:57 -0500

    Creando Store con Pinia

--- vueProject/src/components/SideBar.vue
+++ vueProject/src/components/SideBar.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="sidebar">
-    <h2>Barra Lateral</h2>
+    <h2>Bienvenido</h2>
+    <h3>Nombre: {{ nombre }}</h3>
+    <h3>Email: {{ email }}</h3>
+
     <ul>
       <li>
         <router-link to="/contador">Contador</router-link>
@@ -14,7 +17,26 @@
     </ul>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { useRegistrarStore } from "@/modules/registros/stores/registrarStore.js";
+import { ref, watch } from "vue";
+const registrarStore = useRegistrarStore();
+const nombre = ref(registrarStore.nombre.value);
+const email = ref(registrarStore.email.value);
+watch(
+  () => registrarStore.nombre,
+  (newValue) => {
+    nombre.value = newValue;
+  }
+);
+
+watch(
+  () => registrarStore.email,
+  (newValue) => {
+    email.value = newValue;
+  }
+);
+</script>
 <style>
 .sidebar {
   width: 200px;
